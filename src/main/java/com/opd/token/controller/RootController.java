e87021bd1e21dfb83d39f07f61aa6325169eebdf
Sarla Bharath Chandra <sarlabharathchandra@gmail.com>
2026-01-31 23:59:57 +0530

    Add root endpoint

---
+++ src/main/java/com/opd/token/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.opd.token.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "OPD Token Engine Backend is running";
+    }
+}
